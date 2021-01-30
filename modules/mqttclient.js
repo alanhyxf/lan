b6f3c5c6e505ca192367d178523ff2684f487fce
alanhyxf <alanhyxf@qq.com>
2021-01-30 16:39:48 +0800

    qtt4

--- modules/mqttclient.js
+++ modules/mqttclient.js
@@ -40,68 +40,67 @@ function MqttConn(DeviceInfo,client_sock) {
         password:password,
         client_id:client_id
       };
-    }.then(function(){
-        Client= mqtt.connect(MqttOption.url,{
+    };
+    
+    Client= mqtt.connect(MqttOption.url,{
           username:MqttOption.username,
           password:MqttOption.password,
           clientId:MqttOption.clientid
-      }).then(function(){
-        
-          Client.on('message', function (topic, message) {
-            // message is Buffer
-            console.log(topic+':'+message.toString());
-            //client.end();
-            //message:{"method":"action","clientToken":"1ea48e5644ce4582b1558b8e8926e3e6","actionId":"CAM","timestamp":1611666092,"params":{"action":0,"http_url":"www","count":1}}
-            //var TopicObj=JSON.parse(topic);
-            var IOTObj=JSON.parse(message);
-            if(IOTObj.method=="action"){
-      
-                //return IOTObj.actionId;
-                //拍照指令
-                if(IOTObj.actionId=="CAM"){  
-                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":4,\"timestamp\":%s,\"action\":%d,\"http_url\":\"%s\",\"count\":%d}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.action,IOTObj.params.http_url,IOTObj.params.count);
-                  client_sock.write(sformat);
-                };
-      
-                //配置
-                if(IOTObj.actionId=="CONFIG"){  
-                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":52,\"timestamp\":%s,\"conn_id\":0,\"app\":\"%s\",\"host\":\"%s\",\"port\":%d,\"opt\":%d,\"inteval\":%s,\"upload_url\":\"%s\",\"audio_vol\":%d,\"led_level\":%d,\"at_cmds\":\"%s\"}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.app,IOTObj.params.host,IOTObj.params.port,IOTObj.params.opt,IOTObj.params.inteval,IOTObj.params.upload_url,IOTObj.params.audio_vol,IOTObj.params.led_level,IOTObj.params.at_cmds);
-                  client_sock.write(sformat);
-                };
-                //重启
-                if(IOTObj.actionId=="Reboot"){  
-                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":50,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
-                  client_sock.write(sformat);
-                };
-      
-                if(IOTObj.actionId=="CAMNow"){  
-                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":6,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
-                  client_sock.write(sformat);
-                };
-      
-                if(IOTObj.actionId=="Update"){  
-                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":8,\"timestamp\":%s,\"firmware_url\":%s,\"update_version\":%s,\"firmware_md5\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.firmware_url,IOTObj.params.update_version,IOTObj.params.firmare_md5);
-                  client_sock.write(sformat);
-                };
-                if(IOTObj.actionId=="UploadLog"){  
-                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":10,\"timestamp\":%s,\"http_url\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.http_url);
-                  client_sock.write(sformat);
-                };
-            };
-          });
-      
-          Client.on('connect', function (topic, message) {
-            // message is Buffer
-            //console.log(topic+':'+message.toString());    
-            var topic1='$thing/down/property/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
-            var topic2='$thing/down/action/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
-            Client.subscribe(topic1);	
-            Client.subscribe(topic2);	
-          });
-
-
       });
+        
+    Client.on('message', function (topic, message) {
+      // message is Buffer
+      console.log(topic+':'+message.toString());
+      //client.end();
+      //message:{"method":"action","clientToken":"1ea48e5644ce4582b1558b8e8926e3e6","actionId":"CAM","timestamp":1611666092,"params":{"action":0,"http_url":"www","count":1}}
+      //var TopicObj=JSON.parse(topic);
+      var IOTObj=JSON.parse(message);
+      if(IOTObj.method=="action"){
+
+          //return IOTObj.actionId;
+          //拍照指令
+          if(IOTObj.actionId=="CAM"){  
+            var sformat=util.format("C28C0DB26D39331A{\"msg_type\":4,\"timestamp\":%s,\"action\":%d,\"http_url\":\"%s\",\"count\":%d}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.action,IOTObj.params.http_url,IOTObj.params.count);
+            client_sock.write(sformat);
+          };
+
+          //配置
+          if(IOTObj.actionId=="CONFIG"){  
+            var sformat=util.format("C28C0DB26D39331A{\"msg_type\":52,\"timestamp\":%s,\"conn_id\":0,\"app\":\"%s\",\"host\":\"%s\",\"port\":%d,\"opt\":%d,\"inteval\":%s,\"upload_url\":\"%s\",\"audio_vol\":%d,\"led_level\":%d,\"at_cmds\":\"%s\"}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.app,IOTObj.params.host,IOTObj.params.port,IOTObj.params.opt,IOTObj.params.inteval,IOTObj.params.upload_url,IOTObj.params.audio_vol,IOTObj.params.led_level,IOTObj.params.at_cmds);
+            client_sock.write(sformat);
+          };
+          //重启
+          if(IOTObj.actionId=="Reboot"){  
+            var sformat=util.format("C28C0DB26D39331A{\"msg_type\":50,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
+            client_sock.write(sformat);
+          };
+
+          if(IOTObj.actionId=="CAMNow"){  
+            var sformat=util.format("C28C0DB26D39331A{\"msg_type\":6,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
+            client_sock.write(sformat);
+          };
+
+          if(IOTObj.actionId=="Update"){  
+            var sformat=util.format("C28C0DB26D39331A{\"msg_type\":8,\"timestamp\":%s,\"firmware_url\":%s,\"update_version\":%s,\"firmware_md5\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.firmware_url,IOTObj.params.update_version,IOTObj.params.firmare_md5);
+            client_sock.write(sformat);
+          };
+          if(IOTObj.actionId=="UploadLog"){  
+            var sformat=util.format("C28C0DB26D39331A{\"msg_type\":10,\"timestamp\":%s,\"http_url\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.http_url);
+            client_sock.write(sformat);
+          };
+      };
     });
+      
+    Client.on('connect', function (topic, message) {
+      // message is Buffer
+      //console.log(topic+':'+message.toString());    
+      var topic1='$thing/down/property/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
+      var topic2='$thing/down/action/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
+      Client.subscribe(topic1);	
+      Client.subscribe(topic2);	
+    });
+
+
 
     function randomString(len, charSet) {
       charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';

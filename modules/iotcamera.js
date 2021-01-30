e5820f5797edbc20266c1917b84b1df91418590e
alanhyxf <alanhyxf@qq.com>
2021-01-30 22:42:55 +0800

    mqtt

--- modules/iotcamera.js
+++ modules/iotcamera.js
@@ -52,101 +52,7 @@ module.exports = function (app) {
 
 
     function MqttInit(DeviceInfo){     
-      console.log('1');
-      if(DeviceInfo.mqtt_status==0){ 
-        console.log('2');
-        var mqtt    = require('mqtt');
-
-        var MqttOption=function(DeviceInfo){
-          let product_id = DeviceInfo.product_id;
-          let device_name = DeviceInfo.device_name;
-          let device_secret = DeviceInfo.device_secret;
-          var signmethod = 'HMAC-SHA256';
-          var connid = randomString(5);
-          var expiry = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7;
-          let client_id = product_id + device_name;
-          let username = client_id + ';' + '12010126' + ';' + connid + ';' + expiry;
-          let token = '';
-          let password = '';
-          if (signmethod === 'HMAC-SHA1') {
-            token=cryptojs.HmacSHA1(username, cryptojs.enc.Base64.parse(device_secret))
-            password = token + ';' + 'hmacsha1'
-          } else {
-              token=cryptojs.HmacSHA256(username, cryptojs.enc.Base64.parse(device_secret))
-              password = token + ';' + 'hmacsha256'
-          }
-          return   {
-            url:'mqtt://'+product_id+'.iotcloud.tencentdevices.com',
-            username:username,
-            password:password,
-            client_id:client_id
-          };
-        }
-
-        var mqttclient  = mqtt.connect(MqttOption.url,{
-          username:MqttOption.username,
-          password:MqttOption.password,
-          clientId:MqttOption.clientid
-        },function(){
-          console.log('3');
-        }
-        
-        );
-
-        mqttclient.on('connect', function () {
-          //订阅presence主题
-          DeviceInfo.mqtt_status=1;
-          var topic1='$thing/down/property/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
-          var topic2='$thing/down/action/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
-          mqttclient.subscribe(topic1);	
-          mqttclient.subscribe(topic2);	
-          console.log('4');
-        });
-         
-        mqttclient.on('message', function (topic, message) {
-          //收到的消息是一个Buffer
-          console.log(message.toString());
-
-          var IOTObj=JSON.parse(message);
-          if(IOTObj.method=="action"){
-
-              //return IOTObj.actionId;
-              //拍照指令
-              if(IOTObj.actionId=="CAM"){  
-                var sformat=util.format("C28C0DB26D39331A{\"msg_type\":4,\"timestamp\":%s,\"action\":%d,\"http_url\":\"%s\",\"count\":%d}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.action,IOTObj.params.http_url,IOTObj.params.count);
-                client_sock.write(sformat);
-              };
-
-              //配置
-              if(IOTObj.actionId=="CONFIG"){  
-                var sformat=util.format("C28C0DB26D39331A{\"msg_type\":52,\"timestamp\":%s,\"conn_id\":0,\"app\":\"%s\",\"host\":\"%s\",\"port\":%d,\"opt\":%d,\"inteval\":%s,\"upload_url\":\"%s\",\"audio_vol\":%d,\"led_level\":%d,\"at_cmds\":\"%s\"}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.app,IOTObj.params.host,IOTObj.params.port,IOTObj.params.opt,IOTObj.params.inteval,IOTObj.params.upload_url,IOTObj.params.audio_vol,IOTObj.params.led_level,IOTObj.params.at_cmds);
-                client_sock.write(sformat);
-              };
-              //重启
-              if(IOTObj.actionId=="Reboot"){  
-                var sformat=util.format("C28C0DB26D39331A{\"msg_type\":50,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
-                client_sock.write(sformat);
-              };
-
-              if(IOTObj.actionId=="CAMNow"){  
-                var sformat=util.format("C28C0DB26D39331A{\"msg_type\":6,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
-                client_sock.write(sformat);
-              };
-
-              if(IOTObj.actionId=="Update"){  
-                var sformat=util.format("C28C0DB26D39331A{\"msg_type\":8,\"timestamp\":%s,\"firmware_url\":%s,\"update_version\":%s,\"firmware_md5\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.firmware_url,IOTObj.params.update_version,IOTObj.params.firmare_md5);
-                client_sock.write(sformat);
-              };
-              if(IOTObj.actionId=="UploadLog"){  
-                var sformat=util.format("C28C0DB26D39331A{\"msg_type\":10,\"timestamp\":%s,\"http_url\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.http_url);
-                client_sock.write(sformat);
-              };
-          };
-          //client.end();
-        });
-        
-          
-      }
+ 
       
       
 
@@ -267,16 +173,103 @@ module.exports = function (app) {
       //将来升级为注册指令，可以转换为msg_type处理。
        //已经注册过的设备
       var oldDevice = function (DeviceInfo) {
-        console.log("Device Exist："+DeviceInfo.device_id);
-        //然后根据数据包类型进行转换 msg_type： 1 心跳包 3 抓拍reply  5 长链接抓拍reply  7 升级包reply 51 配置reply
+        console.log("Device Exist1："+DeviceInfo.device_id);
         
-        
-        MqttInit(DeviceInfo);
+        //MqttInit(DeviceInfo);
+
+        console.log('1');
+        if(DeviceInfo.mqtt_status==0){ 
+          console.log('2');
+          var mqtt    = require('mqtt');
+  
+          var MqttOption=function(DeviceInfo){
+            let product_id = DeviceInfo.product_id;
+            let device_name = DeviceInfo.device_name;
+            let device_secret = DeviceInfo.device_secret;
+            var signmethod = 'HMAC-SHA256';
+            var connid = randomString(5);
+            var expiry = Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7;
+            let client_id = product_id + device_name;
+            let username = client_id + ';' + '12010126' + ';' + connid + ';' + expiry;
+            let token = '';
+            let password = '';
+            if (signmethod === 'HMAC-SHA1') {
+              token=cryptojs.HmacSHA1(username, cryptojs.enc.Base64.parse(device_secret))
+              password = token + ';' + 'hmacsha1'
+            } else {
+                token=cryptojs.HmacSHA256(username, cryptojs.enc.Base64.parse(device_secret))
+                password = token + ';' + 'hmacsha256'
+            }
+            return   {
+              url:'mqtt://'+product_id+'.iotcloud.tencentdevices.com',
+              username:username,
+              password:password,
+              client_id:client_id
+            };
+          }
+  
+          var mqttclient  = mqtt.connect(MqttOption.url,{
+              username:MqttOption.username,
+              password:MqttOption.password,
+              clientId:MqttOption.clientid
+            },function(){
+            console.log('3');
+            }
+          );
+  
+          mqttclient.on('connect', function () {
+            //订阅presence主题
+            DeviceInfo.mqtt_status=1;
+            var topic1='$thing/down/property/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
+            var topic2='$thing/down/action/'+DeviceInfo.product_id+'/'+DeviceInfo.device_name;
+            mqttclient.subscribe(topic1);	
+            mqttclient.subscribe(topic2);	
+            console.log('4');
+          });
+           
+          mqttclient.on('message', function (topic, message) {
+            //收到的消息是一个Buffer
+            console.log(message.toString());
+  
+            var IOTObj=JSON.parse(message);
+            if(IOTObj.method=="action"){
+                //return IOTObj.actionId;
+                //拍照指令
+                if(IOTObj.actionId=="CAM"){  
+                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":4,\"timestamp\":%s,\"action\":%d,\"http_url\":\"%s\",\"count\":%d}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.action,IOTObj.params.http_url,IOTObj.params.count);
+                  client_sock.write(sformat);
+                };
+  
+                //配置
+                if(IOTObj.actionId=="CONFIG"){  
+                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":52,\"timestamp\":%s,\"conn_id\":0,\"app\":\"%s\",\"host\":\"%s\",\"port\":%d,\"opt\":%d,\"inteval\":%s,\"upload_url\":\"%s\",\"audio_vol\":%d,\"led_level\":%d,\"at_cmds\":\"%s\"}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.app,IOTObj.params.host,IOTObj.params.port,IOTObj.params.opt,IOTObj.params.inteval,IOTObj.params.upload_url,IOTObj.params.audio_vol,IOTObj.params.led_level,IOTObj.params.at_cmds);
+                  client_sock.write(sformat);
+                };
+                //重启
+                if(IOTObj.actionId=="Reboot"){  
+                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":50,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
+                  client_sock.write(sformat);
+                };
+  
+                if(IOTObj.actionId=="CAMNow"){  
+                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":6,\"timestamp\":%s}15B86F2D013B2618",parseInt(+new Date()/1000));
+                  client_sock.write(sformat);
+                };
+  
+                if(IOTObj.actionId=="Update"){  
+                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":8,\"timestamp\":%s,\"firmware_url\":%s,\"update_version\":%s,\"firmware_md5\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.firmware_url,IOTObj.params.update_version,IOTObj.params.firmare_md5);
+                  client_sock.write(sformat);
+                };
+                if(IOTObj.actionId=="UploadLog"){  
+                  var sformat=util.format("C28C0DB26D39331A{\"msg_type\":10,\"timestamp\":%s,\"http_url\":%s}15B86F2D013B2618",parseInt(+new Date()/1000),IOTObj.params.http_url);
+                  client_sock.write(sformat);
+                };
+            };
+            //client.end();
+          });  
+        }
+
         ReplyMessage(dataobj.msg_type,DeviceInfo);
-      
- 
-       
-        
       };
 
       var newDevice = function (DeviceInfo) {
